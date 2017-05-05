35f3d437d5c96e8772835d81fc419628904a190f
Unknown <bengmartin@outlook.com>
2017-05-05 16:03:58 +1000

    Changed Heading
    Changed web page heading to placeholder

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
   <nav>
-    <h1>Index Page</h1> 
+    <h1>ParkFind</h1> 
     <ul id="navlist">
       <li><a href="index.php"><img width="100" height="100" src="bench_outline.png"><br />Home</a></li>
       <li><a href="registration.php"><img width="100" height="100" src="dogwalk_outline.png"><br />Registration</a></li>
